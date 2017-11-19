42eac53f968d4dd3bb3acc7049e627c3f3db0a49
milleva <emiller.arkko@gmail.com>
2017-11-19 14:18:56 +0200

    megadev

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,21 @@
+var formVisible = false;
+
 function theTeam(){
     empty();
     document.getElementById('mainContent').innerHTML =
         'Evan Miller - Full Stack Developer';
+    document.getElementById('mainPicture').style.visibility = 'visible';
     document.getElementById('mainPicture').src = '../images/evan_represent_working_smile1.jpg';
     document.getElementById('content1').innerHTML =
         'Studies Computer Science in the University of Helsinki.<br><br>' +
         'Programming experience in java, javascript and C.<br><br>' +
-        'Strengths include logical thinking and hard work<br><br>' +
-        '';
+        'Loves to code, play chess and tennis on his freetime.';
+    document.getElementById('secondTeamMemberName').innerHTML =
+        'Axel Wallin - Backend Developer';
     document.getElementById('content2').innerHTML =
-        '';
+        '3rd year Computer Science student at the University of Helsinki.<br><br>' +
+        'Excellent coding skills with Java in particular.<br><br>' +
+        'Interested in music, chess and pharmacology.';
 }
 
 function showContact(){
@@ -18,6 +24,8 @@ function showContact(){
         'Country: Finland<br>' +
         'eMail: evanmillersolutions@gmail.com<br>' +
         'phone: +358 40 812 6244';
+    document.getElementById('orText').style.visibility = 'visible';
+    document.getElementById('mail').style.visibility = 'visible';
 }
 
 function whatnow(){
@@ -37,15 +45,19 @@ function about(){
         'aspiring Computer Science student Evan Miller.<br><br>' +
         '<spans style="color:cornflowerblue">Services include</spans> mobile, web and general application development for private customers as well as businesses. ' +
         'For example homepages, inventories, blogging apps or anything you can come up with!<br><br>' +
-        '<span style="color:pink"><b>It goes without saying</b></span> that because we are a business run by students, prices are adjusted accordingly. ' +
+        '<span style="color:hotpink"><b>It goes without saying</b></span> that because we are a business run by students, prices are adjusted accordingly. ' +
         'Easily affordable coding services are still hard to come across. This is where you need to look no further!';
 
 }
 
 function empty(){
-    document.getElementById('mainPicture').src = '../images/black.png';
+    document.getElementById('mainPicture').style.visibility = 'hidden';
     document.getElementById('content1').innerHTML = '';
+    document.getElementById('content2').innerHTML = '';
     document.getElementById('mainContent').innerHTML = '';
     document.getElementById('topContent').innerHTML = '';
     document.getElementById('contact').innerHTML = '';
+    document.getElementById('secondTeamMemberName').innerHTML = '';
+    document.getElementById('orText').style.visibility = 'hidden';
+    document.getElementById('mail').style.visibility = 'hidden';
 }
