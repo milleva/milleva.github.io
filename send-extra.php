f0aadeb8ef764a6b9dba3f9926004852dfdb71b6
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2017-12-29 18:28:44 +0200

    email send

--- send-extra.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $name = $_POST['name'] ;
-  $email = $_POST['email'] ;
-  $details = $_POST['details'];
-  $message = $name."\n".$email."\n".$details;//\n
-  mail("evanmillersolutions@gmail.com", "Mail", $message);
-?>
